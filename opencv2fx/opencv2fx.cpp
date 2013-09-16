fc2d6ed2f0f54d6c1d52fa48d8c69dca5feec348
Frédéric Devernay <frederic.devernay@m4x.org>
2013-09-16 22:04:46 +0200

    more checks for bad hosts

--- opencv2fx/opencv2fx.cpp
+++ opencv2fx/opencv2fx.cpp
@@ -1,5 +1,62 @@
 #include "opencv2fx.h"
 
+#include <iostream>
+#include <stdexcept>
+
+namespace OFX {
+    /** @brief Throws an @ref OFX::Exception depending on the status flag passed in */
+    void throwSuiteStatusException(OfxStatus stat) throw(OFX::Exception::Suite, std::bad_alloc)
+    {
+        switch (stat)
+        {
+            case kOfxStatOK :
+            case kOfxStatReplyYes :
+            case kOfxStatReplyNo :
+            case kOfxStatReplyDefault :
+                break;
+
+            case kOfxStatErrMemory :
+                throw std::bad_alloc();
+
+            default :
+                std::cout << "Threw suite exception!" << std::endl;
+                throw OFX::Exception::Suite(stat);
+        }
+    }
+
+    void throwHostMissingSuiteException(std::string name) throw(OFX::Exception::Suite)
+    {
+        std::cout << "Threw suite exception! Host missing '" << name << "' suite." << std::endl;
+        throw OFX::Exception::Suite(kOfxStatErrUnsupported);
+    }
+
+
+    /** @brief maps status to a string */
+    const char* mapStatusToString(OfxStatus stat)
+    {
+        switch(stat)
+        {
+            case kOfxStatOK             : return "kOfxStatOK";
+            case kOfxStatFailed         : return "kOfxStatFailed";
+            case kOfxStatErrFatal       : return "kOfxStatErrFatal";
+            case kOfxStatErrUnknown     : return "kOfxStatErrUnknown";
+            case kOfxStatErrMissingHostFeature : return "kOfxStatErrMissingHostFeature";
+            case kOfxStatErrUnsupported : return "kOfxStatErrUnsupported";
+            case kOfxStatErrExists      : return "kOfxStatErrExists";
+            case kOfxStatErrFormat      : return "kOfxStatErrFormat";
+            case kOfxStatErrMemory      : return "kOfxStatErrMemory";
+            case kOfxStatErrBadHandle   : return "kOfxStatErrBadHandle";
+            case kOfxStatErrBadIndex    : return "kOfxStatErrBadIndex";
+            case kOfxStatErrValue       : return "kOfxStatErrValue";
+            case kOfxStatReplyYes       : return "kOfxStatReplyYes";
+            case kOfxStatReplyNo        : return "kOfxStatReplyNo";
+            case kOfxStatReplyDefault   : return "kOfxStatReplyDefault";
+            case kOfxStatErrImageFormat : return "kOfxStatErrImageFormat";
+        }
+        return "UNKNOWN STATUS CODE";
+    }
+}
+
 void defineDoubleParam( OfxPropertySuiteV1 *gPropHost,
 			OfxParameterSuiteV1 *gParamHost,
 			OfxParamSetHandle effectParams,
@@ -10,17 +67,28 @@ void defineDoubleParam( OfxPropertySuiteV1 *gPropHost,
 			double max,
 			double initial)
 {
-  OfxParamHandle param;
+  //OfxParamHandle param;
   OfxPropertySetHandle props;
-  gParamHost->paramDefine(effectParams, kOfxParamTypeDouble, name, &props);
+  OfxStatus stat;
+
+  stat = gParamHost->paramDefine(effectParams, kOfxParamTypeDouble, name, &props);
+  OFX::throwSuiteStatusException(stat);
 
   // say we are a scaling parameter
-  gPropHost->propSetString(props, kOfxParamPropDoubleType, 0, kOfxParamDoubleTypeScale);
-  gPropHost->propSetDouble(props, kOfxParamPropDefault, 0, initial);
-  gPropHost->propSetDouble(props, kOfxParamPropMin, 0, 0.0);
-  gPropHost->propSetDouble(props, kOfxParamPropDisplayMin, 0, min);
-  gPropHost->propSetDouble(props, kOfxParamPropDisplayMax, 0, max);
-  gPropHost->propSetString(props, kOfxParamPropHint, 0, hint);
-  gPropHost->propSetString(props, kOfxParamPropScriptName, 0, name);
-  gPropHost->propSetString(props, kOfxPropLabel, 0, label);
+  stat = gPropHost->propSetString(props, kOfxParamPropDoubleType, 0, kOfxParamDoubleTypeScale);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetDouble(props, kOfxParamPropDefault, 0, initial);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetDouble(props, kOfxParamPropMin, 0, 0.0);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetDouble(props, kOfxParamPropDisplayMin, 0, min);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetDouble(props, kOfxParamPropDisplayMax, 0, max);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetString(props, kOfxParamPropHint, 0, hint);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetString(props, kOfxParamPropScriptName, 0, name);
+  OFX::throwSuiteStatusException(stat);
+  stat = gPropHost->propSetString(props, kOfxPropLabel, 0, label);
+  OFX::throwSuiteStatusException(stat);
 }
--- opencv2fx/opencv2fx.h
+++ opencv2fx/opencv2fx.h
@@ -29,4 +29,108 @@ clamp(T v, int min, int max)
   return v;
 }
 
+
+/** @brief The core 'OFX Support' namespace, used by plugin implementations. All code for these are defined in the common support libraries.
+ */
+namespace OFX {
+    /** @brief maps a status to a string for debugging purposes, note a c-str for printf */
+    const char * mapStatusToString(OfxStatus stat);
+
+    /** @brief namespace for OFX support lib exceptions, all derive from std::exception, calling it */
+    namespace Exception {
+
+        /** @brief thrown when a suite returns a dud status code
+         */
+        class Suite : public std::exception {
+            protected :
+            OfxStatus _status;
+            public :
+            Suite(OfxStatus s) : _status(s) {}
+            OfxStatus status(void) {return _status;}
+            operator OfxStatus() {return _status;}
+
+            /** @brief reimplemented from std::exception */
+            virtual const char * what () const throw () {return mapStatusToString(_status);}
+
+        };
+
+        /** @brief Exception indicating that a host doesn't know about a property that is should do */
+        class PropertyUnknownToHost : public std::exception {
+            protected :
+            std::string _what;
+            public :
+            PropertyUnknownToHost(const char *what) : _what(what) {}
+            virtual ~PropertyUnknownToHost() throw() {}
+
+            /** @brief reimplemented from std::exception */
+            virtual const char * what () const throw ()
+            {
+                return _what.c_str();
+            }
+        };
+
+        /** @brief exception indicating that the host thinks a property has an illegal value */
+        class PropertyValueIllegalToHost : public std::exception {
+            protected :
+            std::string _what;
+            public :
+            PropertyValueIllegalToHost(const char *what) : _what(what) {}
+            virtual ~PropertyValueIllegalToHost() throw() {}
+
+            /** @brief reimplemented from std::exception */
+            virtual const char * what () const throw ()
+            {
+                return _what.c_str();
+            }
+        };
+
+        /** @brief exception indicating a request for a named thing exists (eg: a param), but is of the wrong type, should never make it back to the main entry
+         indicates a logical error in the code. Asserts are raised in debug code in these situations.
+         */
+        class TypeRequest : public std::exception {
+            protected :
+            std::string _what;
+            public :
+            TypeRequest(const char *what) : _what(what) {}
+            virtual ~TypeRequest() throw() {}
+
+            /** @brief reimplemented from std::exception */
+            virtual const char * what () const throw ()
+            {
+                return _what.c_str();
+            }
+        };
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // These exceptions are to be thrown by the plugin if it hits a problem, the
+        // code managing the main entry will trap the exception and return a suitable
+        // status code to the host.
+
+        /** @brief exception indicating a required host feature is missing */
+        class HostInadequate : public std::exception {
+            protected :
+            std::string _what;
+            public :
+            HostInadequate(const char *what) : _what(what) {}
+            virtual ~HostInadequate() throw() {}
+            
+            /** @brief reimplemented from std::exception */
+            virtual const char * what () const throw ()
+            {
+                return _what.c_str();
+            }
+        };
+        
+    }; // end of Exception namespace
+
+    /** @brief Throws an @ref OFX::Exception::Suite depending on the status flag passed in */
+    void
+    throwSuiteStatusException(OfxStatus stat)
+    throw(OFX::Exception::Suite, std::bad_alloc);
+
+    void
+    throwHostMissingSuiteException(std::string name)
+    throw(OFX::Exception::Suite);
+};
+
 #endif
