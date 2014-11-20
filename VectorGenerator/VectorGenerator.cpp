36d9a0ae60bd7307b8654467ab75828f0e375a6b
Alexandre Gauthier <alexandre230@msn.com>
2014-11-20 13:56:12 +0100

    Reorganise the tree, add an empty GenericCVPlugin definition + VectorGenerator plug-in

---
+++ VectorGenerator/VectorGenerator.cpp
@@ -0,0 +1,423 @@
+/*
+ OFX Clamp plugin.
+
+ Copyright (C) 2014 INRIA
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+
+ Neither the name of the {organization} nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ INRIA
+ Domaine de Voluceau
+ Rocquencourt - B.P. 105
+ 78153 Le Chesnay Cedex - France
+
+
+ The skeleton for this source file is from:
+ OFX Invert Example plugin, a plugin that illustrates the use of the OFX Support library.
+
+ Copyright (C) 2007 The Open Effects Association Ltd
+ Author Bruno Nicoletti bruno@thefoundry.co.uk
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ * Neither the name The Open Effects Association Ltd, nor the names of its
+ contributors may be used to endorse or promote products derived from this
+ software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The Open Effects Association Ltd
+ 1 Wardour St
+ London W1D 6PA
+ England
+
+
+ */
+
+
+#ifdef _WINDOWS
+#include <windows.h>
+#endif
+
+#include "GenericCVPlugin.h"
+#include "ofxsMultiThread.h"
+#include "ofxsMaskMix.h"
+
+
+#define kPluginName "VectorGeneratorOFX"
+#define kPluginGrouping "Time"
+#define kPluginDescription ""
+#define kPluginIdentifier "net.sf.openfx.VectorGenerator"
+#define kPluginVersionMajor 1 // Incrementing this number means that you have broken backwards compatibility of the plug-in.
+#define kPluginVersionMinor 0 // Increment this when you have fixed a bug or made it faster.
+
+#define kSupportsTiles 0
+#define kSupportsMultiResolution 1
+#define kSupportsRenderScale 1
+#define kRenderThreadSafety eRenderFullySafe
+
+
+using namespace OFX;
+
+class VectorGeneratorPlugin : public GenericCVPlugin
+{
+  public:
+    /** @brief ctor */
+    VectorGeneratorPlugin(OfxImageEffectHandle handle)
+            : GenericCVPlugin(handle)
+    {
+        maskClip_ = getContext() == OFX::eContextFilter ? NULL : fetchClip(getContext() == OFX::eContextPaint ? "Brush" : "Mask");
+        assert(!maskClip_ || maskClip_->getPixelComponents() == ePixelComponentAlpha);
+    
+    }
+
+  private:
+    /* Override the render */
+    virtual void render(const OFX::RenderArguments &args) OVERRIDE FINAL;
+
+    /* set up and run a processor */
+    void setupAndProcess(ClampBase &, const OFX::RenderArguments &args);
+
+    virtual bool isIdentity(const IsIdentityArguments &args, Clip * &identityClip, double &identityTime) OVERRIDE FINAL;
+
+    /** @brief called when a clip has just been changed in some way (a rewire maybe) */
+    virtual void changedClip(const InstanceChangedArgs &args, const std::string &clipName) OVERRIDE FINAL;
+
+  private:
+    // do not need to delete these, the ImageEffect is managing them for us
+    OFX::Clip *maskClip_;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/** @brief render for the filter */
+
+////////////////////////////////////////////////////////////////////////////////
+// basic plugin render function, just a skelington to instantiate templates from
+
+
+/* set up and run a processor */
+void
+ClampPlugin::setupAndProcess(ClampBase &processor, const OFX::RenderArguments &args)
+{
+    // get a dst image
+    std::auto_ptr<OFX::Image> dst(dstClip_->fetchImage(args.time));
+    if (!dst.get()) {
+        OFX::throwSuiteStatusException(kOfxStatFailed);
+    }
+    OFX::BitDepthEnum dstBitDepth       = dst->getPixelDepth();
+    OFX::PixelComponentEnum dstComponents  = dst->getPixelComponents();
+
+    // fetch main input image
+    std::auto_ptr<OFX::Image> src(srcClip_->fetchImage(args.time));
+
+    // make sure bit depths are sane
+    if (src.get()) {
+        OFX::BitDepthEnum    srcBitDepth      = src->getPixelDepth();
+        OFX::PixelComponentEnum srcComponents = src->getPixelComponents();
+
+        // see if they have the same depths and bytes and all
+        if (srcBitDepth != dstBitDepth || srcComponents != dstComponents) {
+            OFX::throwSuiteStatusException(kOfxStatErrImageFormat);
+        }
+    }
+
+    // auto ptr for the mask.
+    std::auto_ptr<OFX::Image> mask((getContext() != OFX::eContextFilter) ? maskClip_->fetchImage(args.time) : 0);
+
+    // do we do masking
+    if (getContext() != OFX::eContextFilter && maskClip_->isConnected()) {
+        bool maskInvert;
+        _maskInvert->getValueAtTime(args.time, maskInvert);
+
+        // say we are masking
+        processor.doMasking(true);
+
+        // Set it in the processor
+        processor.setMaskImg(mask.get(), maskInvert);
+    }
+
+    bool red, green, blue, alpha;
+    _processR->getValueAtTime(args.time, red);
+    _processG->getValueAtTime(args.time, green);
+    _processB->getValueAtTime(args.time, blue);
+    _processA->getValueAtTime(args.time, alpha);
+    RGBAValues minimum(0.);
+    _minimum->getValueAtTime(args.time, minimum.r, minimum.g, minimum.b, minimum.a);
+    bool minimumEnable;
+    _minimumEnable->getValueAtTime(args.time, minimumEnable);
+    RGBAValues maximum(1.);
+    _maximum->getValueAtTime(args.time, maximum.r, maximum.g, maximum.b, maximum.a);
+    bool maximumEnable;
+    _maximumEnable->getValueAtTime(args.time, maximumEnable);
+    RGBAValues minClampTo(0.);
+    _minClampTo->getValueAtTime(args.time, minClampTo.r, minClampTo.g, minClampTo.b, minClampTo.a);
+    bool minClampToEnable;
+    _minClampToEnable->getValueAtTime(args.time, minClampToEnable);
+    RGBAValues maxClampTo(1.);
+    _maxClampTo->getValueAtTime(args.time, maxClampTo.r, maxClampTo.g, maxClampTo.b, maxClampTo.a);
+    bool maxClampToEnable;
+    _maxClampToEnable->getValueAtTime(args.time, maxClampToEnable);
+    bool premult;
+    int premultChannel;
+    _premult->getValueAtTime(args.time, premult);
+    _premultChannel->getValueAtTime(args.time, premultChannel);
+    double mix;
+    _mix->getValueAtTime(args.time, mix);
+    processor.setValues(red, green, blue, alpha,
+                        minimum, minimumEnable, maximum, maximumEnable,
+                        minClampTo, minClampToEnable, maxClampTo, maxClampToEnable,
+                        premult, premultChannel, mix);
+
+    // set the images
+    processor.setDstImg(dst.get());
+    processor.setSrcImg(src.get());
+
+    // set the render window
+    processor.setRenderWindow(args.renderWindow);
+
+    // Call the base class process member, this will call the derived templated process code
+    processor.process();
+}
+
+// the overridden render function
+void
+ClampPlugin::render(const OFX::RenderArguments &args)
+{
+    // instantiate the render code based on the pixel depth of the dst clip
+    OFX::BitDepthEnum       dstBitDepth    = dstClip_->getPixelDepth();
+    OFX::PixelComponentEnum dstComponents  = dstClip_->getPixelComponents();
+
+    // do the rendering
+    if (dstComponents == OFX::ePixelComponentRGBA) {
+        switch (dstBitDepth) {
+            case OFX::eBitDepthUByte : {
+                ImageClamper<unsigned char, 4, 255> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthUShort : {
+                ImageClamper<unsigned short, 4, 65535> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthFloat : {
+                ImageClamper<float, 4, 1> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+            default :
+                OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+        }
+    } else if (dstComponents == OFX::ePixelComponentRGB) {
+        switch (dstBitDepth) {
+            case OFX::eBitDepthUByte : {
+                ImageClamper<unsigned char, 3, 255> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthUShort : {
+                ImageClamper<unsigned short, 3, 65535> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthFloat : {
+                ImageClamper<float, 3, 1> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+            default :
+                OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+        }
+    } else {
+        assert(dstComponents == OFX::ePixelComponentAlpha);
+        switch (dstBitDepth) {
+            case OFX::eBitDepthUByte : {
+                ImageClamper<unsigned char, 1, 255> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthUShort : {
+                ImageClamper<unsigned short, 1, 65535> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+
+            case OFX::eBitDepthFloat : {
+                ImageClamper<float, 1, 1> fred(*this);
+                setupAndProcess(fred, args);
+            }
+                break;
+            default :
+                OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+        }
+    }
+}
+
+bool
+ClampPlugin::isIdentity(const IsIdentityArguments &args, Clip * &identityClip, double &/*identityTime*/)
+{
+    bool red, green, blue, alpha;
+    double mix;
+    _processR->getValueAtTime(args.time, red);
+    _processG->getValueAtTime(args.time, green);
+    _processB->getValueAtTime(args.time, blue);
+    _processA->getValueAtTime(args.time, alpha);
+    _mix->getValueAtTime(args.time, mix);
+
+    if (mix == 0. || (!red && !green && !blue && !alpha)) {
+        identityClip = srcClip_;
+        return true;
+    }
+
+    bool minimumEnable, maximumEnable;
+    _minimumEnable->getValueAtTime(args.time, minimumEnable);
+    _maximumEnable->getValueAtTime(args.time, maximumEnable);
+
+    if (!minimumEnable && !maximumEnable) {
+        identityClip = srcClip_;
+        return true;
+    }
+
+    return false;
+}
+
+void
+ClampPlugin::changedClip(const InstanceChangedArgs &args, const std::string &clipName)
+{
+    if (clipName == kOfxImageEffectSimpleSourceClipName && srcClip_ && args.reason == OFX::eChangeUserEdit) {
+        switch (srcClip_->getPreMultiplication()) {
+            case eImageOpaque:
+                _premult->setValue(false);
+                break;
+            case eImagePreMultiplied:
+                _premult->setValue(true);
+                break;
+            case eImageUnPreMultiplied:
+                _premult->setValue(false);
+                break;
+        }
+    }
+}
+
+mDeclarePluginFactory(VectorGeneratorPluginFactory, {}, {});
+
+using namespace OFX;
+void VectorGeneratorPluginFactory::describe(OFX::ImageEffectDescriptor &desc)
+{
+    // basic labels
+    desc.setLabels(kPluginName, kPluginName, kPluginName);
+    desc.setPluginGrouping(kPluginGrouping);
+    desc.setPluginDescription(kPluginDescription);
+
+    // add the supported contexts
+    desc.addSupportedContext(eContextFilter);
+    desc.addSupportedContext(eContextGeneral);
+    desc.addSupportedContext(eContextPaint);
+
+    // add supported pixel depths
+    desc.addSupportedBitDepth(eBitDepthUByte);
+    desc.addSupportedBitDepth(eBitDepthUShort);
+    desc.addSupportedBitDepth(eBitDepthFloat);
+
+    // set a few flags
+    desc.setSingleInstance(false);
+    desc.setHostFrameThreading(false);
+    desc.setSupportsMultiResolution(kSupportsMultiResolution);
+    desc.setSupportsTiles(kSupportsTiles);
+    desc.setTemporalClipAccess(false);
+    desc.setRenderTwiceAlways(false);
+    desc.setSupportsMultipleClipPARs(false);
+    desc.setRenderThreadSafety(kRenderThreadSafety);
+
+}
+
+void VectorGeneratorPluginFactory::describeInContext(OFX::ImageEffectDescriptor &desc, OFX::ContextEnum context)
+{
+    // Source clip only in the filter context
+    // create the mandated source clip
+    ClipDescriptor *srcClip = desc.defineClip(kOfxImageEffectSimpleSourceClipName);
+    srcClip->addSupportedComponent(ePixelComponentRGBA);
+    srcClip->addSupportedComponent(ePixelComponentRGB);
+    srcClip->addSupportedComponent(ePixelComponentAlpha);
+    srcClip->setTemporalClipAccess(false);
+    srcClip->setSupportsTiles(kSupportsTiles);
+    srcClip->setIsMask(false);
+    
+    // create the mandated output clip
+    ClipDescriptor *dstClip = desc.defineClip(kOfxImageEffectOutputClipName);
+    dstClip->addSupportedComponent(ePixelComponentRGBA);
+    dstClip->addSupportedComponent(ePixelComponentRGB);
+    dstClip->addSupportedComponent(ePixelComponentAlpha);
+    dstClip->setSupportsTiles(kSupportsTiles);
+
+    if (context == eContextGeneral || context == eContextPaint) {
+        ClipDescriptor *maskClip = context == eContextGeneral ? desc.defineClip("Mask") : desc.defineClip("Brush");
+        maskClip->addSupportedComponent(ePixelComponentAlpha);
+        maskClip->setTemporalClipAccess(false);
+        if (context == eContextGeneral) {
+            maskClip->setOptional(true);
+        }
+        maskClip->setSupportsTiles(kSupportsTiles);
+        maskClip->setIsMask(true);
+    }
+
+    // make some pages and to things in
+    PageParamDescriptor *page = desc.definePageParam("Controls");
+
+}
+
+OFX::ImageEffect* VectorGeneratorPluginFactory::createInstance(OfxImageEffectHandle handle, OFX::ContextEnum /*context*/)
+{
+    return new ClampPlugin(handle);
+}
+
+
+void getVectorGeneratorPluginID(OFX::PluginFactoryArray &ids)
+{
+    static VectorGeneratorPluginFactory p(kPluginIdentifier, kPluginVersionMajor, kPluginVersionMinor);
+    ids.push_back(&p);
+}
+
